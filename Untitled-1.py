f38da6e0a0458fe6ccc9b2f7dfb53e30387ef28c
Richard Cuadrado2 <cuadradorichard@gmail.com>
2024-10-03 20:04:30 -0500

    Prueba

---
+++ Untitled-1.py
@@ -0,0 +1,12 @@
+
+print ("")   
+for i in range (9):
+        print (" "*i, end="")
+        for j in range (i, 9):
+            print (j, end= " ")
+        print ("")
+for i in range (7,-1,-1):
+        print (" "*i, end="")
+        for j in range (8,i-1,-1):
+            print (j, end= " ")
+        print ("")
